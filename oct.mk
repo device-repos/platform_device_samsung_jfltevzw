5df9fa6cde82f4c4eed259a585bd732291f4ded3
treken <chupekevin@gmail.com>
2013-11-24 04:06:15 -0500

    Add oct make file

--- oct.mk
+++ oct.mk
@@ -1,16 +1,16 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-# Inherit some common TG stuff.
-$(call inherit-product, vendor/Gummy/config/cdma.mk)
+# Inherit some common OCT stuff.
+$(call inherit-product, vendor/oct/config/cdma.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/Gummy/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
 
-# Inherit some common TG stuff.
-$(call inherit-product, vendor/Gummy/config/common_full_phone.mk)
+# Inherit some common OCT stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
 
-PRODUCT_NAME := tg_jfltevzw
+PRODUCT_NAME := oct_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 
