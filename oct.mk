990af322e5b58bc70d6224cc0815bda7716d526b
treken <chupekevin@gmail.com>
2013-11-24 21:54:50 -0500

    Remove cm stuff and prepare for OctOS

--- oct.mk
+++ oct.mk
@@ -1,16 +1,5 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-# Inherit some common OCT stuff.
-$(call inherit-product, vendor/oct/config/cdma.mk)
-
-# Enhanced NFC
-$(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
-
-# Inherit some common OCT stuff.
-$(call inherit-product, vendor/oct/config/common_full_phone.mk)
-
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
-
 PRODUCT_NAME := oct_jfltevzw
 PRODUCT_DEVICE := jfltevzw
 
