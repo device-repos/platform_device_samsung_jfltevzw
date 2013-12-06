cf46d4165a783d4af5f28ee2224f68a431257629
Carl Windhausen <cjwindhausen@gmail.com>
2013-12-06 10:49:18 -0500

    Did someone say 4.4.1_r1? KOT49E

--- oct.mk
+++ oct.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/jfltevzw/full_jfltevzw.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="Verizon/jfltevzw/jfltevzw:4.2.2/JDQ39/I545VRUAMDK:user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.2.2 JDQ39 I545VRUAMDK release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevzw TARGET_DEVICE=jfltevzw BUILD_FINGERPRINT="samsung/oct_jfltevzw/jfltevzw:4.4.1/KOT49E/user/release-keys" PRIVATE_BUILD_DESC="jfltevzw-user 4.4.1 KOT49E release-keys"
 
 PRODUCT_NAME := oct_jfltevzw
 PRODUCT_DEVICE := jfltevzw
